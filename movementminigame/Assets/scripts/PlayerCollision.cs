8b8889c0fc3910cb75c76a48e6a2389896904725
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2018-09-25 15:51:48 -0400

    add new file

---
+++ movementminigame/Assets/scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public playercontrol movement;
+
+
+     void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Bottom")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }  
+    }
+
+}
