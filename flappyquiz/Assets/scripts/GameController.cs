88e86bd50a7ecb759c5a346e019f11c63ce9175f
KorvusZ017 <42786143+KorvusZ017@users.noreply.github.com>
2018-10-18 18:36:24 -0400

    uploading files

---
+++ flappyquiz/Assets/scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public Text scoreText;
+    int score = 0;
+
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    
+}
---
+++ flappyquiz/Assets/scripts/jump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class jump : MonoBehaviour {
+    Rigidbody2D rb;
+    public Text scoreText;
+    private int score = 0;
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        
+
+    }
+	
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0f, 5f, 0f); 
+        }
+
+
+	}
+
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.CompareTag("scorepoint"))
+        {
+            score = score + 1;
+
+        }
+    }
+}
